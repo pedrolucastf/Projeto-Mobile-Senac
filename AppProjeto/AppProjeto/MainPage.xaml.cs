bcd3ddf13700fb53b543399cef648d5bda80e9bd
Pedro Lucas <pedrolucastf@hotmail.com>
2019-02-20 16:55:40 -0300

    Funcionalidade dos botões e menu hamburguer.

--- AppProjeto/AppProjeto/MainPage.xaml.cs
+++ AppProjeto/AppProjeto/MainPage.xaml.cs
@@ -14,24 +14,30 @@ namespace AppProjeto
             InitializeComponent();
         }
 
-        private void ButtonEntrar_Clicked(object sender, EventArgs e)
+        private async void ButtonEntrar_Clicked(object sender, EventArgs e)
         {
-
+            if (entryLogin.Text == "admin" && entrySenha.Text == "admin")
+            {
+                //Abre página master do aplicativo. 
+                var page = new MenuMaster();
+                await Navigation.PushModalAsync(page);
+            }
+            else
+            {
+                await DisplayAlert("Ops...", "Login e/ou senha incorretos!", "OK");
+            }
         }
 
-        private void ButtonCancelar_Clicked(object sender, EventArgs e)
+        private async void ButtonNovoUsuario_Clicked(object sender, EventArgs e)
         {
-
+            var cadastro = new CadastroUsuarios();
+            await Navigation.PushModalAsync(cadastro);
         }
 
-        private void ButtonNovoUsuario_Clicked(object sender, EventArgs e)
+        private async void ButtonEsqueciSenha_Clicked(object sender, EventArgs e)
         {
-
-        }
-
-        private void ButtonEsqueciSenha_Clicked(object sender, EventArgs e)
-        {
-
+            var recuperarsenha = new RecuperarSenha();
+            await Navigation.PushModalAsync(recuperarsenha);
         }
     }
 }
---
+++ AppProjeto/AppProjeto/MenuMaster.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppProjeto
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuMaster : MasterDetailPage
+    {
+        public MenuMaster()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new TelaInicio());
+        }
+
+        private void ViewCell_Tapped(object sender, EventArgs e)
+        {
+            Detail.Navigation.PushAsync(new Computador());
+            IsPresented = false;
+        }
+
+        private void ViewCell_Tapped_1(object sender, EventArgs e)
+        {
+            Detail.Navigation.PushAsync(new Notebook());
+            IsPresented = false;
+        }
+
+        private void ViewCell_Tapped_2(object sender, EventArgs e)
+        {
+            Detail.Navigation.PushAsync(new TV());
+            IsPresented = false;
+        }
+    }
+}
